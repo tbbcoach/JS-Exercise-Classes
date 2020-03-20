fc027f7be694aca3c5c8bdc17fa72ba17324eb57
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-03-20 14:54:51 -0400

    started second task.

--- index.js
+++ index.js
@@ -76,7 +76,18 @@ toString() {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill = function(gallons) {
+    return (this.tank += gallons);
+}
+  drive = function() {
+    
+  }
 }
 
 /*
